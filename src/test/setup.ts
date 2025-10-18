4db5406cada4f11ffa004dce0c79aa2b00a5d057
jalal-haidar <jalal99.devl@gmail.com>
2025-10-19 04:24:58 +0500

    ci-fix-vitest

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,4 +1,9 @@
 // Vitest setup file - register jest-dom matchers with Vitest's expect
-import '@testing-library/jest-dom';
+import * as matchers from '@testing-library/jest-dom/matchers';
+import { expect } from 'vitest';
+
+// extend Vitest's expect with jest-dom matchers
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+expect.extend(matchers as any);
 
 export {};

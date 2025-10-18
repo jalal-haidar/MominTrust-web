2e86b94eab2468a32891cda5f2d5570b15a88706
jalal-haidar <jalal99.devl@gmail.com>
2025-10-19 03:35:52 +0500

    chore: fix dev tooling — add eslint react detect pin prettier and add prettier cjs config

---
+++ .prettierrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: 'all',
+  // register plugin explicitly for compatibility with Prettier v3
+  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
+  importOrder: [
+    "^(^react$|@react|react|^recoil$)",
+    "^@mui/(.*)$",
+    "<THIRD_PARTY_MODULES>",
+    "^@/(.*)$",
+    "^[./]"
+  ],
+  importOrderGroupNamespaceSpecifiers: true,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};

41773203c8816caae527252704226c196486bee0
jalal-haidar <jalal99.devl@gmail.com>
2025-10-19 00:18:47 +0500

    fix: update path import to use 'node:path' in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import * as path from 'path';
+import * as path from 'node:path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
